18ad64598479e117f1e727449139ec2abac16e59
Md Taseen Atehar <63313585+Seen3@users.noreply.github.com>
2021-09-25 00:09:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let im=document.getElementById("ala");
+im.addEventListener("mouseover",()=>{
+    im.src="https://s.yimg.com/ny/api/res/1.2/e38BfgfTxVmxive04rNypw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTM3OS42NjEwMTY5NDkxNTI1NQ--/https://s.yimg.com/uu/api/res/1.2/8neFXo7PkPGwV3X5eEGXgQ--~B/aD0zNTA7dz01OTA7YXBwaWQ9eXRhY2h5b24-/http://media.zenfs.com/en-SG/homerun/thepoppingpost.com/e4d56a6668b8ea48755eeeec91422a78";
+})
+
+im.addEventListener("mouseout",()=>{
+    im.src="https://scontent-del1-1.xx.fbcdn.net/v/t31.18172-8/10498220_680138548728068_484863159246656692_o.jpg?_nc_cat=110&ccb=1-5&_nc_sid=e3f864&_nc_ohc=3Ll6DJkEeScAX_MjfZn&_nc_ht=scontent-del1-1.xx&oh=b590bf55d5045e66aa243abd2b69bcaa&oe=61720557";
+})
+
